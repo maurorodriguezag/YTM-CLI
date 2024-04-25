627fd1da74987eff1504e7692905eeb571fd4232
Andrés Mauricio Rodríguez González <maurocore@Laptop-de-Andres.local>
2024-04-24 23:15:59 -0500

    Actualizacion evitar cierre con cmd q

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const { app, BrowserWindow, Tray, Menu, protocol, screen } = require('electron');
-const path = require('path');
+const {
+  app,
+  BrowserWindow,
+  Tray,
+  Menu,
+  protocol,
+  screen,
+  globalShortcut,
+} = require("electron");
+const path = require("path");
 
 let mainWindow;
 let tray;
@@ -16,12 +24,13 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadURL('https://youtube.com');
+  mainWindow.loadURL("https://youtube.com");
   const { width } = screen.getPrimaryDisplay().workAreaSize;
   const x = width - (mainWindow.getBounds().width - 20);
   const y = 0;
+
   mainWindow.setPosition(x, y);
-  mainWindow.on('close', function (event) {
+  mainWindow.on("close", function (event) {
     if (app.quitting) {
       mainWindow = null;
     } else {
@@ -30,7 +39,7 @@ function createWindow() {
     }
   });
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (mainWindow === null) {
       createWindow();
     } else {
@@ -39,60 +48,70 @@ function createWindow() {
   });
 
   // Agregar el ícono a la barra de notificaciones
-  const iconPath = path.join(__dirname, 'icon_bartool.png'); // Reemplaza 'path-to-your-icon.png' con la ruta a tu icono
+  const iconPath = path.join(__dirname, "icon_bartool.png"); // Reemplaza 'path-to-your-icon.png' con la ruta a tu icono
   tray = new Tray(iconPath);
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: 'Mostrar Aplicación',
+      label: "Mostrar Aplicación",
       click: () => {
         mainWindow.show();
       },
     },
     {
-      label: 'Salir',
+      label: "Salir",
       click: () => {
         app.quitting = true;
-        app.exit();  // Cambiado de app.quit() a app.exit()
+        app.exit(); // Cambiado de app.quit() a app.exit()
       },
     },
   ]);
-  
+
   // Manejar doble clic en el ícono para mostrar/ocultar la ventana
-  tray.on('right-click', () => {
+  tray.on("right-click", () => {
     contextMenu.popup(mainWindow);
   });
 
   // Manejar doble clic en el ícono para mostrar/ocultar la ventana
-  tray.on('click', () => {
+  tray.on("click", () => {
     mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
   });
 }
 
-app.on('ready', () => {
+app.on("ready", () => {
   createWindow();
 
+  mainWindow.on("focus", () => {
+    globalShortcut.register("CommandOrControl+Q", () => {
+      mainWindow.hide();
+    });
+  });
+
+  mainWindow.on("blur", () => {
+    globalShortcut.unregister("CommandOrControl+Q");
+  });
+
   // Configuración para ocultar la aplicación en el dock en macOS
-  if (process.platform === 'darwin') {
+  if (process.platform === "darwin") {
     app.dock.hide();
 
-    protocol.registerFileProtocol('app', (request, callback) => {
-      const url = request.url.replace('app://', '');
+    protocol.registerFileProtocol("app", (request, callback) => {
+      const url = request.url.replace("app://", "");
       callback({ path: path.normalize(`${__dirname}/${url}`) });
     });
 
-    app.setAsDefaultProtocolClient('app');
+    app.setAsDefaultProtocolClient("app");
   }
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.exit();
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.exit();
 });
 
-app.on('activate', function () {
+app.on("activate", function () {
   if (mainWindow === null) createWindow();
 });
 
 // Manejar el evento 'before-quit' para limpiar la bandeja
-app.on('before-quit', () => {
+app.on("before-quit", () => {
   tray.destroy();
 });
